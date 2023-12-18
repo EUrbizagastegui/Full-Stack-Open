5a5c02f9754bacaca170b11828fb2b7462392de4
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-12-17 20:02:31 -0500

    Completado el ejercicio 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,19 @@ const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const Counter = ({text, count}) => <div>{text} {count}</div>
 
+const Statistics = ({feedback, findAvarege, findPositive}) => {
+  return (
+    <div>
+      <Counter text={'good'} count={feedback.good}></Counter>
+      <Counter text={'neutral'} count={feedback.neutral}></Counter>
+      <Counter text={'bad'} count={feedback.bad}></Counter>
+      <Counter text={'all'} count={feedback.good + feedback.neutral + feedback.bad}></Counter>
+      <Counter text={'average'} count={findAvarege()}></Counter>
+      <Counter text={'positive'} count={findPositive() + '%'}></Counter>
+    </div>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [feedback, setFeedback] = useState({
@@ -36,12 +49,7 @@ const App = () => {
       <Button text={'neutral'} onClick={() => handleFeedback('neutral')} />
       <Button text={'bad'} onClick={() => handleFeedback('bad')} />
       <Display text={'statistics'} />
-      <Counter text={'good'} count={feedback.good}></Counter>
-      <Counter text={'neutral'} count={feedback.neutral}></Counter>
-      <Counter text={'bad'} count={feedback.bad}></Counter>
-      <Counter text={'all'} count={total}></Counter>
-      <Counter text={'average'} count={findAvarege()}></Counter>
-      <Counter text={'positive'} count={findPositive() + '%'}></Counter>
+      <Statistics feedback={feedback} findAvarege={findAvarege} findPositive={findPositive}></Statistics>
     </div>
   )
 }
